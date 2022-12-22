f7969b5bc43ac16d293272a57b3209b949c30bf1
Wotw-Potp <s.otsuka08@gmail.com>
2022-12-22 22:56:24 +0900

    hotfix env

--- app/front/src/config/Firebase.ts
+++ app/front/src/config/Firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 
 const firebaseConfig: FirebaseOptions = import.meta.env.PROD
   ? {
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      apiKey: process.env.VITE_FIREBASE_API_KEY,
+      authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.VITE_FIREBASE_APP_ID,
     }
   : {
       apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
